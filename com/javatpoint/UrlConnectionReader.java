8be5843b467f109d1bfae510b3dfda3093d40421
Frazer-Nyambe <2018217071@student.unza.zm>
2021-10-28 14:16:21 +0200

    Create UrlConnectionReader.java

---
+++ com/javatpoint/UrlConnectionReader.java
@@ -0,0 +1,37 @@
+package com.javatpoint;  
+import java.net.*;  
+import java.io.*;  
+public class UrlConnectionReader  
+{  
+  public static void main(String[] args)  
+  {  
+    String output  = getUrlContents("https://ictjournal.icict.org.zm/index.php/zictjournal/index");  
+    System.out.println(output);  
+  }  
+  
+  private static String getUrlContents(String theUrl)  
+  {  
+    StringBuilder content = new StringBuilder();  
+  // Use try and catch to avoid the exceptions  
+    try  
+    {  
+      URL url = new URL(theUrl); // creating a url object  
+      URLConnection urlConnection = url.openConnection(); // creating a urlconnection object  
+  
+      // wrapping the urlconnection in a bufferedreader  
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));  
+      String line;  
+      // reading from the urlconnection using the bufferedreader  
+      while ((line = bufferedReader.readLine()) != null)  
+      {  
+        content.append(line + "\n");  
+      }  
+      bufferedReader.close();  
+    }  
+    catch(Exception e)  
+    {  
+      e.printStackTrace();  
+    }  
+    return content.toString();  
+  }  
+}  
